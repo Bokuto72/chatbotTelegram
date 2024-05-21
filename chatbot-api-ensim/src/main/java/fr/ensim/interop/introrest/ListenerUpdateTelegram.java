c1737ea7680639844b59773216dcb703f1ace237
Thibault <t.lantzener@gmail.com>
2024-05-21 18:13:55 +0200

    ajout getUpdates et sendMessage

--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/ListenerUpdateTelegram.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/ListenerUpdateTelegram.java
@@ -1,18 +1,55 @@
 package fr.ensim.interop.introrest;
 
+import fr.ensim.interop.introrest.model.telegram.ApiResponseUpdateTelegram;
+import fr.ensim.interop.introrest.model.telegram.Message;
+import fr.ensim.interop.introrest.model.telegram.Update;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+// TODO
+
+
 @Component
 public class ListenerUpdateTelegram implements CommandLineRunner {
-	
+
+	private Integer updateId = 389257981;
+
 	@Override
 	public void run(String... args) throws Exception {
 		Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "Démarage du listener d'updates Telegram...");
 		
 		// Operation de pooling pour capter les evenements Telegram
+		Timer timer = new Timer();
+		long delay = 5000L;
+		TimerTask timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("Pooling...");
+				ApiResponseUpdateTelegram apiResponseUpdateTelegram = new ApiResponseUpdateTelegram();
+				apiResponseUpdateTelegram.getResult().add(getUpdates(updateId));
+			}
+		};
+		timer.schedule(timerTask, delay);
+	}
+
+	@GetMapping("/getUpdates")
+	public Update getUpdates(@RequestParam("offset") Integer offset) {
+		Update update = new Update();
+		if(updateId != offset) {
+
+		}
+		System.out.println(update.getMessage().getText());
+		return update;
 	}
 }
--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -1,13 +1,24 @@
 package fr.ensim.interop.introrest.controller;
 
+import fr.ensim.interop.introrest.model.telegram.Message;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MessageRestController {
 	
 	@Value("${telegram.api.url}")
 	private String telegramApiUrl;
-	
+	private String telegramApiToken = "6861595645:AAGZl6Ztk5kq9GcnZgrqAizESF_w-0Kj3pg";
+	@Value("${telegram.bot.id}")
+	private String telegramBotId;
+	private long chatId = 6984281278L;
+
 	//Opérations sur la ressource Message
+	@PostMapping("/sendMessage")
+	public ResponseEntity<Message> postMessage(@RequestBody Message message) {
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 }
